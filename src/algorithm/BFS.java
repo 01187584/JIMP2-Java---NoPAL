76bda02d5e51c6c475e719a0eff52ec3d833a27f
Mateusz <mateusz.czyzewski04@outlook.com>
2024-05-27 20:34:26 +0200

    Wstępny interfejs klas rozwiązujących

--- src/algorithm/BFS.java
+++ src/algorithm/BFS.java
@@ -1,5 +1,44 @@
 package algorithm;
 
+import graph_v2.Graph;
+import graph_v2.Vertex;
+
+import java.util.List;
+
 public class BFS implements PathfindingAlgorithm {
-    
+
+    @Override
+    public void initialize(Graph g) {
+
+    }
+
+    @Override
+    public List<List<Vertex>> executeAlgorithm(Vertex start, Vertex end) {
+        return null;
+    }
+
+    @Override
+    public int getSolutionAmount() {
+        return 0;
+    }
+
+    @Override
+    public List<Vertex> getSolution() {
+        return null;
+    }
+
+    @Override
+    public List<Vertex> getSolution(int solutionNumber) {
+        return null;
+    }
+
+    @Override
+    public int getSolutionLength() {
+        return 0;
+    }
+
+    @Override
+    public int getSolutionLength(int solutionNumber) {
+        return 0;
+    }
 }
--- src/algorithm/PathfindingAlgorithm.java
+++ src/algorithm/PathfindingAlgorithm.java
@@ -1,5 +1,21 @@
 package algorithm;
 
+import graph_v2.Graph;
+import graph_v2.Vertex;
+import java.util.List;
+
 public interface PathfindingAlgorithm {
+    void initialize(Graph g);
+    List<List<Vertex>> executeAlgorithm(Vertex start, Vertex end);
+    boolean equals(Object o);
+    int hashCode();
+    int getSolutionAmount();
+    List<Vertex> getSolution();
+    List<Vertex> getSolution(int solutionNumber);
+    int getSolutionLength();
+    int getSolutionLength(int solutionNumber);
+
+
+
     
 }
